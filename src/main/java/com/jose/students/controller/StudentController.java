ef9bb67bad655cf641c9c9c95a039c02fd4e1e30
jsan2321 <jsantiago.c@hotmail.com>
2024-08-24 20:14:42 -0500

    added mapping methods in the controller

--- src/main/java/com/jose/students/controller/StudentController.java
+++ src/main/java/com/jose/students/controller/StudentController.java
@@ -1,9 +1,12 @@
 package com.jose.students.controller;
 
+import com.jose.students.entity.Student;
 import com.jose.students.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -16,7 +19,20 @@ public class StudentController {
 
     @GetMapping({"/students", "/"})
     public String listStudents(Model model) {
+        model.addAttribute("students", service.listAllStudents());
         return "students";
     }
 
+    @GetMapping("/students/new")
+    public String showStudentRegisterForm(Model model) {
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "create_student";
+    }
+
+    @PostMapping("students")
+    public String saveStudent(@ModelAttribute("student") Student student) {
+        service.saveStudent(student);
+        return "redirect:/students";
+    }
 }
